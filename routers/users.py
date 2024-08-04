99148de2243c729dc2e16a9cbec128b4a9fa0342
JSeHoone <beebea545@gmail.com>
2024-08-04 17:05:41 +0900

    Add : Login시 refresh token발급 하는 로직 추가

--- routers/users.py
+++ routers/users.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
+from curd.token import create_user_token_info
 from curd.users import validate_user
 from schema import users
-from utils.authentication import get_access_token
+from utils.authentication import get_access_token, get_refresh_token
 
 router = APIRouter(
     prefix="/api/v1",
@@ -24,6 +25,12 @@ async def login(user_info: users.UserLogin):
         access_token = get_access_token(user_info)
 
         # TODO: Refresh Token Update or Add
+        refresh_token = get_refresh_token()
+        create_user_token_info(
+            user_email=user_info.userEmail,
+            refresh_token=refresh_token,
+        )
+
     else:
         return False
 
